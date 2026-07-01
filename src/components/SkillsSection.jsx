d46b1db5e450ce932deb99ce92090abff9de6335
raghvendra-official <raghvendraofficials@gmail.com>
2026-07-01 14:30:11 +0530

    Adding icon in skill section

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -8,7 +8,41 @@ import {
   FaDatabase,
   FaCloud,
   FaTools,
+  FaAws,
+  FaGitAlt,
+  FaFigma,
+  FaMapMarkedAlt,
+  FaShieldAlt,
+  FaProjectDiagram,
 } from "react-icons/fa";
+
+import {
+  SiJavascript,
+  SiCplusplus,
+  SiHtml5,
+  SiCss,
+  SiReact,
+  SiBootstrap,
+  SiRedux,
+  SiNodedotjs,
+  SiExpress,
+  SiMongodb,
+  SiMysql,
+  SiDocker,
+  SiTerraform,
+  SiAnsible,
+  SiLinux,
+  SiGithub,
+  SiPostman,
+  SiCloudinary,
+} from "react-icons/si";
+
+import {
+  MdOutlineDesignServices,
+
+} from "react-icons/md";
+
+
 
 const icons = {
   "Programming Languages": <FaCode />,
@@ -17,6 +51,58 @@ const icons = {
   Databases: <FaDatabase />,
   "Cloud & DevOps": <FaCloud />,
   "Tools & Platforms": <FaTools />,
+};
+
+const skillIcons = {
+  // Programming
+  "JavaScript": <SiJavascript />,
+  "C++": <SiCplusplus />,
+  "HTML5": <SiHtml5 />,
+  "CSS3": <SiCss />,
+  "SQL": <FaDatabase />,
+
+  // Frontend
+  "React.js": <SiReact />,
+  "Bootstrap": <SiBootstrap />,
+  "Redux": <SiRedux />,
+  "Material UI": <FaLaptopCode />,
+  "EJS": <FaCode />,
+  "Responsive Design": <FaLaptopCode />,
+
+  // Backend
+  "Node.js": <SiNodedotjs />,
+  "Express.js": <SiExpress />,
+  "REST APIs": <FaServer />,
+  "JWT Authentication": <FaShieldAlt />,
+  "MVC Architecture": <FaProjectDiagram />,
+
+  // Database
+  "MongoDB": <SiMongodb />,
+  "MongoDB Atlas": <SiMongodb />,
+  "MySQL": <SiMysql />,
+  "AWS DynamoDB": <FaDatabase />,
+
+  // Cloud & DevOps
+  "AWS EC2": <FaAws />,
+  "AWS S3": <FaAws />,
+  "AWS IAM": <FaAws />,
+  "AWS VPC": <FaAws />,
+  "AWS Amplify": <FaAws />,
+  "Docker": <SiDocker />,
+  "Docker Compose": <SiDocker />,
+  "Terraform": <SiTerraform />,
+  "Ansible": <SiAnsible />,
+  "GitHub Actions": <SiGithub />,
+  "CI/CD": <FaCode />,
+  "Linux": <SiLinux />,
+
+  // Tools
+  "GitHub": <SiGithub />,
+  "VS Code": <FaCode />,
+  "Postman": <SiPostman />,
+  "Cloudinary": <SiCloudinary />,
+  "Mapbox": <FaMapMarkedAlt />,
+  "Figma": <FaFigma />,
 };
 
 export default function SkillsSection() {
@@ -47,6 +133,10 @@ export default function SkillsSection() {
             <div className="skill-list">
               {group.items.map((skill) => (
                 <span key={skill} className="skill-pill">
+                  <span className="skill-pill-icon">
+                    {skillIcons[skill]}
+                  </span>
+
                   {skill}
                 </span>
               ))}
